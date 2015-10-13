ab3e65c343a54dccd4adf70bddd88bd573b72c40
minhngocln <minhngoc.ln@gmail.com>
2015-10-13 10:45:29 -0700

    basic paintbrush functionality implemented

--- app/assets/javascripts/components/canvas.js.jsx
+++ app/assets/javascripts/components/canvas.js.jsx
@@ -1,8 +1,23 @@
 (function(root) {
   'use strict';
   root.Canvas = React.createClass({
+    getInitialState: function () {
+      return { mouseDown: false };
+    },
+
+    toggleClick: function () {
+      console.log("toggle click");
+      if (this.state.mouseDown) {
+        this.setState({ mouseDown: false });
+      } else {
+        this.setState({ mouseDown: true });
+      }
+    },
+
+
     render: function () {
-      var canvas = this.props.drawing;
+      var that = this;
+      var canvas = that.props.drawing;
       var style = {width: ((canvas.size * 15) + (canvas.size * 2))};
       var cells = canvas.content;
 
@@ -11,7 +26,11 @@
              style={style}>
              {
                cells.map(function (cell) {
-                 return <Cell key={cell.id} cell={cell}/>;
+                 return <Cell key={cell.id}
+                              cell={cell}
+                              toggleClick={that.toggleClick}
+                              mouseDown={that.state.mouseDown}
+                              handleHover={that.handleHover}/>;
                })
              }
         </div>
--- app/assets/javascripts/components/cell.js.jsx
+++ app/assets/javascripts/components/cell.js.jsx
@@ -1,12 +1,37 @@
 (function(root) {
   'use strict';
   root.Cell = React.createClass({
+    getInitialState: function() {
+      return { active: false };
+    },
+
+    handleMouseDown: function () {
+      this.setState({ active: true});
+      this.props.toggleClick();
+    },
+
+    handleMouseUp: function () {
+      this.props.toggleClick();
+    },
+
+    mouseOver: function(e) {
+      if (this.props.mouseDown) {
+        this.setState({active: true});
+      }
+    },
+
     render: function () {
+      var style = { backgroundColor: "white" };
+      if (this.state.active) { style.backgroundColor = "black"; }
+
       var cell = this.props.cell;
       return (
         <div className="cell"
-             style={cell.style}
-             idx={cell.id}>
+             style={style}
+             idx={cell.id}
+             onMouseDown={this.handleMouseDown}
+             onMouseUp={this.handleMouseUp}
+             onMouseOver={this.mouseOver}>
         </div>
       );
     }
