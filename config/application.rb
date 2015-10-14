31f2d07d76cd189974f7c7a7783b5f99cba48bad
minhngocln <minhngoc.ln@gmail.com>
2015-10-13 18:59:11 -0700

    trying to fix font link in applicationcss

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Pixelate
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"  
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
   end
 end
