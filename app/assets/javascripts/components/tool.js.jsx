387c2a4d0e59e30238df888c9b6dc3011f0d1cef
minhngocln <minhngoc.ln@gmail.com>
2015-10-23 15:34:24 -0700

    made css for different sizes of brush and eraser

--- app/assets/javascripts/components/tool.js.jsx
+++ app/assets/javascripts/components/tool.js.jsx
@@ -5,11 +5,32 @@
       this.props.handleToolSelection(this.props.toolType);
     },
 
+    hoverOverComponent: function () {
+      var sizeOpts;
+      if (this.props.toolType === 'brush' || this.props.toolType === 'eraser') {
+        sizeOpts = (
+          <div className="size-opts">
+            <div className="small"></div>
+            <div className="medium"></div>
+            <div className="large"></div>
+          </div>
+        );
+      }
+
+      return sizeOpts;
+    },
+
     render: function () {
       var icon_url = '/images/' + this.props.toolType + '.png';
 
       return (
-        <img onClick={this.handleClick} className="tool" src={icon_url}/>
+        <div className="tool-wrap">
+          {this.hoverOverComponent()}
+          <img onClick={this.handleClick}
+               onHover={this.handleHover}
+               className="tool"
+               src={icon_url}/>
+        </div>
       );
     }
   });
